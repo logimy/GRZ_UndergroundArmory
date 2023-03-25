4161966ce124645e6d3f8839bbcef3a727b1d55e
Logimy <artem.grozovsky@gmail.com>
2023-03-25 07:53:34 +0200

    Initial commit

---
+++ Media/lua/server/items/DEV6_Distributions.lua
@@ -0,0 +1,13 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions["list"]["GunStoreShelf"].items, "DEV6.ITEMNAME1");
+table.insert(ProceduralDistributions["list"]["GunStoreShelf"].items, 10);
+
+table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, "DEV6.ITEMNAME1");
+table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, 5);
+
+table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, "DEV6.ITEMNAME1");
+table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, 10);
+
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, "DEV6.ITEMNAME1");
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, 5);
